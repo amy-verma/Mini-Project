f611cff4c382c4357f7e39aaaf3c07346fb95aae
amy-verma <amitjsr8786@gmail.com>
2025-09-10 20:39:03 +0530

    todo

--- new/src/App.jsx
+++ new/src/App.jsx
@@ -1,40 +1,32 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react'
 import './App.css'
 
 const App=()=>{
-
-  const [users,setUsers]=useState([])
-  const [search,setSearch]=useState("")
-
-  const FetchData=async()=>{
-    try{
-      const data=await fetch("https://jsonplaceholder.typicode.com/users")
-      const response=await data.json()
-      console.log(response)
-      setUsers(response)
-    }catch(error){
-      console.log(error);
-    }
+  const [input,setInput]=useState("")
+  const [task,setTask]=useState([])
+  
+  const handleAddButton=()=>{
+    if(input.trim()==="") return
+    setTask((prevTask)=>[...prevTask,input])
+   setInput("")
   }
 
-  useEffect(()=>{
-    FetchData()
-  },[])
-  const handleInputChnage=(e)=>{
-    setSearch(e.target.value)
+  const handleOnChange=(e)=>{
+    setInput(e.target.value)
   }
 
-  const filteredData=users.filter((ele)=>{
-    return ele.name.toLowerCase().includes(search.toLocaleLowerCase())
-  })
-
-  return (<>
-  <div><input type='text' placeholder='Search Here' value={search}  onChange={handleInputChnage}/></div>
-  <div><ul>
-    {filteredData.map((ele)=>{
-    return <li key={ele.id}>{ele.name}</li>
-  })}</ul></div>
-  </>)
+  return (
+  <>
+  <h2>To Do App</h2>
+  <input type='text' placeholder='Enter to do' value={input} onChange={handleOnChange}/>
+  <button onClick={handleAddButton}>Add</button>
+  <div>{
+    task.map((task,index)=>{
+      return <li key={index}>{task}</li>
+    })
+    }</div>
+  </>
+  )
 }
 
 export default App
