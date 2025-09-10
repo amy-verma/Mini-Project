39b80275805c4308f4a254a37569a4fe13676236
amy-verma <amitjsr8786@gmail.com>
2025-09-10 19:28:45 +0530

    new

--- new/src/App.jsx
+++ new/src/App.jsx
@@ -1,9 +1,30 @@
-
 import { useEffect, useState } from 'react';
 import './App.css'
 
 const App=()=>{
+
+  const [users,setUsers]=useState([])
+
+  const FetchData=async()=>{
+    try{
+      const data=await fetch("https://jsonplaceholder.typicode.com/users")
+      const response=await data.json()
+      console.log(response)
+      setUsers(response)
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    FetchData()
+  },[])
+
   return (<>
+  <div><ul>
+    {users.map((ele)=>{
+    return <li key={ele.id}>{ele.name}</li>
+  })}</ul></div>
   </>)
 }
 
